f22cea45d18b4798fde400dae1a7b5a83a462841
NSaneQTip <36085380+NSaneQTip@users.noreply.github.com>
2022-10-18 16:26:32 -0700

    Create laserTripwire.py

---
+++ laserTripwire.py
@@ -0,0 +1,12 @@
+from gpiozero import LightSensor, Buzzer #Import the lightsensor and buzzer functionality from the gpiozero library.
+from time import sleep #Import the time library to allow timing commands.
+
+ldr = LightSensor(4) # Le LightSensor
+buzzer = Buzzer(17) # Le Buzzer
+
+while True:
+	sleep(0.1)
+	if ldr.value < 0.5:
+		buzzer.on()
+	else:
+		buzzer.off()
