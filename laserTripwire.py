3c20542639a16d7c1ec79fd1aa43ec40b4f0c4af
NSaneQTip <36085380+NSaneQTip@users.noreply.github.com>
2022-10-19 10:47:51 -0700

    Update laserTripwire.py

--- laserTripwire.py
+++ laserTripwire.py
@@ -1,8 +1,8 @@
 from gpiozero import LightSensor, Buzzer #Import the lightsensor and buzzer functionality from the gpiozero library.
 from time import sleep #Import the time library to allow timing commands.
 
-ldr = LightSensor(5) # Le LightSensor
-buzzer = Buzzer(17) # Le Buzzer
+ldr = LightSensor(4) # Le LightSensor. ldr is the variable call for the LightSensor.
+buzzer = Buzzer(17) # Le Buzzer. buzzer is the variable call for the Buzzer.
 
 while True:
 	sleep(0.1)
