8f065a0d09d5c39450a7a6248f379b66dc830acd
David-Art <55605061+David-Art@users.noreply.github.com>
2022-11-21 11:23:09 -0800

    11/21/2022 Laser Trip Wire Program Script Updated
    I finalized and updated the programming script to include accurate variable initialization values, and included debugging code lines for testing purposes. The main project script code is updated to include accurate print statements.

--- laserTripwire.py
+++ laserTripwire.py
@@ -2,12 +2,25 @@ from gpiozero import LightSensor, Buzzer #Import the lightsensor and buzzer func
 from time import sleep #Import the time library to allow timing commands.
 
 # LDR (the pin #, the queue used to store values read from the circuit, and the charge limit)
-ldr = LightSensor(4, 5, 0.2) # Le LightSensor. ldr is the variable call for the LightSensor.
+ldr = LightSensor(7, charge_time_limit = 0.1, 0.2) # Le LightSensor. ldr is the variable call for the LightSensor.
 buzzer = Buzzer(17) # Le Buzzer. buzzer is the variable call for the Buzzer.
 
+### DEBUGGER ###
+# while True:
+	# print(ldr.value)
+	# sleep(0.5)
+	
+### Buzzer Component Test ###
+# buzzer.on()
+# buzzer.off()
+	
+
+
+# Main Project Script
 while True:
 	sleep(0.1)
 	if ldr.value < 0.5:
 		buzzer.on()
+		print("Intruder Alert! The enemy has taken the intelligence.")
 	else:
 		buzzer.off()
